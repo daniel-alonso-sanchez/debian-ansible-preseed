39b527122852b569e7c6b6f857fc120aba3acec9
daniel-alonso-sanchez <daniel.alonso.sanchez@outlook.es>
2020-12-11 08:57:42 +0000

    uso de imagen docker externa en vez de propia

--- init.sh
+++ init.sh
@@ -2,11 +2,16 @@
 export current_ip="$(hostname -I |awk '{ print $1 }')"
 cp /home/usuario/ansible/local_inventory /home/usuario/ansible/local_inventory_bck
 sed -i "s/127\.0\.0\.1/$current_ip/" /home/usuario/ansible/local_inventory
-echo "Building ansible docker image"
-docker build . -t ansibled  
-echo "Running ansible customization"
-docker run -v /home/usuario/ansible:/ansible/playbooks ansibled install.yml -i local_inventory
-docker run -v /home/usuario/ansible:/ansible/playbooks ansibled post_java_install.yml -i local_inventory
+
+docker run -it --rm \
+    -v ${PWD}:/ansible \
+    jones2748/alpine-ansible-mitogen:latest \
+    ansible-playbook -i local_inventory install.yml
+
+docker run -it --rm \
+    -v ${PWD}:/ansible \
+    jones2748/alpine-ansible-mitogen:latest \
+    ansible-playbook -i local_inventory post_java_install.yml
 
 touch /home/usuario/.initialized
 # dejamos el fichero como estaba
