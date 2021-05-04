2e89b5c383382ee2d36ea2921af0148d6d556fce
daniel-alonso-sanchez <daniel.alonso.sanchez@outlook.es>
2021-05-04 07:42:21 +0200

    Create mkhomedir.sh

---
+++ mkhomedir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+bash -c "cat > /usr/share/pam-configs/mkhomedir" <<EOF
+Name: activate mkhomedir
+Default: yes
+Priority: 900
+Session-Type: Additional
+Session:
+        required                        pam_mkhomedir.so umask=0022 skel=/etc/skel
+EOF; \
