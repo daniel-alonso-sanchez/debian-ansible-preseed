583935b2b465b27084ead5b5a4d47d7c0aa17212
daniel-alonso-sanchez <daniel.alonso.sanchez@outlook.es>
2021-05-06 11:49:39 +0200

    Update ad_config.sh

--- ad_config.sh
+++ ad_config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cat /root/ad_cred| realm join -U admin365 riskms.es
 wget https://raw.githubusercontent.com/daniel-alonso-sanchez/debian-ansible-preseed/master/mkhomedir -O /usr/share/pam-configs/mkhomedir 
 pam-auth-update --enable mkhomedir
 sed -i 's|use_fully_qualified_names = True|use_fully_qualified_names = False|g' /etc/sssd/sssd.conf
