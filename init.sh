0cbee134e14246b8ef14d4cc1a8c62f77935b91f
daniel-alonso-sanchez <daniel.alonso.sanchez@outlook.es>
2021-05-07 12:05:05 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export current_ip="$(hostname -I |awk '{ print $1 }')"
-cp /home/usuario/ansible/local_inventory /home/usuario/ansible/local_inventory_bck
-sed -i "s/127\.0\.0\.1/$current_ip/" /home/usuario/ansible/local_inventory
+cp /home/ansible/local_inventory /home/ansible/local_inventory_bck
+sed -i "s/127\.0\.0\.1/$current_ip/" /home/ansible/local_inventory
 
 docker run \
     -v ${PWD}:/ansible \
@@ -13,7 +13,7 @@ docker run \
     jones2748/alpine-ansible-mitogen:latest \
     ansible-playbook -i local_inventory post_java_install.yml
 
-touch /home/usuario/.initialized
+touch /home/ansible/.initialized
 # dejamos el fichero como estaba
-mv /home/usuario/ansible/local_inventory_bck /home/usuario/ansible/local_inventory
+mv /home/ansible/ansible/local_inventory_bck /home/usuario/ansible/local_inventory
 
