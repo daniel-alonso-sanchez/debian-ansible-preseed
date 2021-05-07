0ed6da50520c7ef9815e90046dc44af1b38086fc
daniel-alonso-sanchez <daniel.alonso.sanchez@outlook.es>
2021-05-07 12:06:32 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export current_ip="$(hostname -I |awk '{ print $1 }')"
-cp /home/ansible/local_inventory /home/ansible/local_inventory_bck
-sed -i "s/127\.0\.0\.1/$current_ip/" /home/ansible/local_inventory
+cp /home/ansible/ansible/local_inventory /home/ansible/ansible/local_inventory_bck
+sed -i "s/127\.0\.0\.1/$current_ip/" /home/ansible/ansible/local_inventory
 
 docker run \
     -v ${PWD}:/ansible \
