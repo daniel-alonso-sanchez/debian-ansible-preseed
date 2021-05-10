53f55bd543cba005f50c91cdba1f2bfa7cde3c9f
daniel-alonso-sanchez <daniel.alonso.sanchez@outlook.es>
2021-05-10 09:55:38 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,12 +5,12 @@ sed -i "s/127\.0\.0\.1/$current_ip/" /home/ansible/ansible/local_inventory
 
 docker run \
     -v ${PWD}:/ansible \
-    jones2748/alpine-ansible-mitogen:latest \
+    willhallonline/ansible:2.9-alpine \
     ansible-playbook -i local_inventory install.yml
 
 docker run \
     -v ${PWD}:/ansible \
-    jones2748/alpine-ansible-mitogen:latest \
+    willhallonline/ansible:2.9-alpine \
     ansible-playbook -i local_inventory post_java_install.yml
 
 touch /home/ansible/.initialized
