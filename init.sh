6baf0b8cf30a5b7abd9368ef19d6e38c38527c4f
daniel-alonso-sanchez <daniel.alonso.sanchez@outlook.es>
2021-05-10 10:30:12 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,12 +3,12 @@ export current_ip="$(hostname -I |awk '{ print $1 }')"
 cp /home/ansible/ansible/local_inventory /home/ansible/ansible/local_inventory_bck
 sed -i "s/127\.0\.0\.1/$current_ip/" /home/ansible/ansible/local_inventory
 
-docker run \
+sudo docker run \
     -v ${PWD}:/ansible \
     willhallonline/ansible:2.9-alpine \
     ansible-playbook -i local_inventory install.yml
 
-docker run \
+sudo docker run \
     -v ${PWD}:/ansible \
     willhallonline/ansible:2.9-alpine \
     ansible-playbook -i local_inventory post_java_install.yml
