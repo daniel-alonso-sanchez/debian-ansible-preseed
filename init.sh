94afff04317d1ccab3cdf29507e220f07209efa0
dalonso <daniel.sanchez@work4data.com>
2021-05-20 12:12:06 +0200

    Eliminado checkout no necesario

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export current_ip="$(hostname -I |awk '{ print $1 }')"
 cp /home/ansible/ansible/local_inventory /home/ansible/ansible/local_inventory_bck
-git checkout -- .
-git checkout -b ad origin/ad
 sed -i "s/127\.0\.0\.1/$current_ip/" /home/ansible/ansible/local_inventory
 sed "s/$/ ansible_ssh_common_args='-o StrictHostKeyChecking=no'/" /home/ansible/ansible/local_inventory > /home/ansible/ansible/local_inventory
 
