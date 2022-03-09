10f3fd237e218457d01cfeb25f9dac14dc3b9b88
daniel-alonso-sanchez <daniel.alonso.sanchez@outlook.es>
2022-03-09 16:51:33 +0100

    Update ad_config.sh

--- ad_config.sh
+++ ad_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/daniel-alonso-sanchez/debian-ansible-preseed/master/mkhomedir -O /usr/share/pam-configs/mkhomedir 
-wget https://raw.githubusercontent.com/daniel-alonso-sanchez/debian-ansible-preseed/master/ad-connect -O /etc/init.d/ad-connect 
+#wget https://raw.githubusercontent.com/daniel-alonso-sanchez/debian-ansible-preseed/master/mkhomedir -O /usr/share/pam-configs/mkhomedir 
+#wget https://raw.githubusercontent.com/daniel-alonso-sanchez/debian-ansible-preseed/master/ad-connect -O /etc/init.d/ad-connect 
 chmod +x /etc/init.d/ad-connect
 update-rc.d ad-connect defaults
